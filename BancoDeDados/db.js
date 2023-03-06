2fe00b6e070db954cb29b205f000402e19910ed3
Luiz <luis.p.borba@hotmail.com>
2023-03-06 11:25:36 -0300

    remoção banco de dados

--- BancoDeDados/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = requie("sequelize");
-const sequelize = new Sequelize("userData","root","root",{
-    dialect:"mysql",
-    host:"localhost",
-    port:3306
-
-})
-modules.exports =sequelize;
